1e75b0df85a0ffcf6d80ad1ee8279e784ff84388
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-11-23 11:41:28 +0900

    first commit. Book Entity, Author Entity, BookRepository

---
+++ src/main/java/com/jpa/exercise/domain/Author.java
@@ -0,0 +1,20 @@
+package com.jpa.exercise.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "book2")
+public class Author {
+    @Id
+    private Long id;
+    private String name;
+}
